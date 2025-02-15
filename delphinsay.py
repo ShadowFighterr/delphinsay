bb70e24b8761e89e4ef4a454abd307a44c2b4828
Azamat Ubaidullauly <154382969+ShadowFighterr@users.noreply.github.com>
2025-02-15 21:50:05 +0500

    Update delphinsay.py

--- delphinsay.py
+++ delphinsay.py
@@ -1,19 +1,12 @@
 def delphinsay(message):
     dolphin = r"""
-         \
-          \
-             .  .
-              \ | /
-           '-.;;;.-'....
-          ----------==;;;;==-
-           .-';;;'-....
-              / | \
-             '  '
-    """
-    # Determine the border length based on the message
+    .
+    .
+    .
+    Dolphin."""
     border_length = len(message) + 4
-    top_border = " " + "_" * border_length
-    bottom_border = " " + "-" * border_length
+    top_border = "_" * border_length
+    bottom_border = "-" * border_length
 
     # Build the text box
     text_box = f"""
@@ -22,7 +15,8 @@ def delphinsay(message):
 {bottom_border}
 """
 
-    print(text_box + dolphin)
+    print(text_box)
+    print(dolphin)
 
 
 delphinsay(input())
